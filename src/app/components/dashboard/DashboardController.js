a9758ff2e373d7d5710ff3d03a114df4358311eb
Sebastian Balay <snbalay@gmail.com>
2015-11-20 01:03:15 -0300

    _this

--- src/app/components/dashboard/DashboardController.js
+++ src/app/components/dashboard/DashboardController.js
@@ -1,5 +1,31 @@
 angular.module('app').controller('DashboardController', [
   function () {
 
+    this.soyElController = true;
+
+    const fn = () => {
+      console.log('this en fn');
+      console.log(this);
+
+      const fn1 = function () {
+        console.log('this en fn1');
+        console.log(this);
+
+        const fn2 = () => {
+          console.log('this en fn2');
+          console.log(this);
+
+        };
+
+        fn2();
+
+      };
+
+      fn1();
+
+    };
+
+    fn();
+
   }
 ]);
